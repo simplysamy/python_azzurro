65808feca3b982fcccf7b543575dd0d1ac6016ce
Sameer Hasan <simplysamy@gmail.com>
2024-10-24 11:58:42 +0530

    Type Hinting

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-text = "this is a text" # string
+# Type Hints
+name: str = 'John'
+number: int = 10
 
-number = 5 # int
-
-decimal_number = 5.5 # float
-
-complex_number = 5j # complex
-
-
-people = ["John", "Michael", "Terry", "Eric", "Graham"] # list
-
-lotto_number = (4, 8, 15, 16, 23, 42) # tuple
-
-num_range = range(1, 100) # range. Generate number from 1-99
-
-
-user = {"username1": "john", "username2": "sam"} # dictionary
-
-unique_numbers = {1, 2, 2, 4, 4} # set. 
-# Only unique values. 
-# Output: {1, 2, 4} 
-# Doesn't allow duplicates. Doesn't allow order.
-
-unique_numbers2 = frozenset({1, 2, 2, 4, 4}) # frozen set. Frozen nad can't be altered
-
-boolean = True # boolean    
-
-is_empty = None # No value associated with is_empty
+# With Type Hints we make our program more explicit
