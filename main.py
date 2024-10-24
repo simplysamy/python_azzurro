307858569b644b91c2df8bc0bb433f2223a98034
Sameer Hasan <simplysamy@gmail.com>
2024-10-24 11:54:53 +0530

    Data Types introduction

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-# constants
-# Naming convnetion. Keep every thing in uppercase
+text = "this is a text" # string
+
+number = 5 # int
+
+decimal_number = 5.5 # float
+
+complex_number = 5j # complex
 
 
-PI = 3.1415
-print(PI)
+people = ["John", "Michael", "Terry", "Eric", "Graham"] # list
 
-PI = 222
-print(PI) # Value will be changed
- # But we should never change the value of a constant
+lotto_number = (4, 8, 15, 16, 23, 42) # tuple
+
+num_range = range(1, 100) # range. Generate number from 1-99
+
+
+user = {"username1": "john", "username2": "sam"} # dictionary
+
+unique_numbers = {1, 2, 2, 4, 4} # set. 
+# Only unique values. 
+# Output: {1, 2, 4} 
+# Doesn't allow duplicates. Doesn't allow order.
+
+unique_numbers2 = frozenset({1, 2, 2, 4, 4}) # frozen set. Frozen nad can't be altered
+
+boolean = True # boolean    
+
+is_empty = None # No value associated with is_empty
