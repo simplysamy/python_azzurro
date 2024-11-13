4fbb0fc77a05cb31f671cdabac7738f24bfb36b6
Sameer Hasan <simplysamy@gmail.com>
2024-11-13 20:23:53 +0530

    Tuple count & index

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-people: tuple = ('John', 'Paul', 'George', 'Ringo') # Best practice to use pranthesis
+people: tuple = ('John', 'John', 'Paul', 'George', 'Ringo') # Best practice to use pranthesis
 
-print(people[2:]) # Print elements from index 2 to the end
-print(people[:2]) # Print elements below 2
+print(people.count('John'))
+
+print(people.index('John')) # Get the index of an element
