1e9f5f8f8453e3e32745c54c70dace684acb6e1f
Sameer Hasan <simplysamy@gmail.com>
2024-11-13 18:09:25 +0530

    Strings

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-numbers = [1,2,3,4,5]
+text1 = 'This is a text'
+text2 = 'This is the "car".'
+text3 =  'This is the new line\nok'
 
-print(1 in numbers) # True
+long_text = """This is a long text
+with multiple lines"""
 
-print(6 in numbers) # FALSE
+ecs_char = "My name is \"sameer\""
 
-
-
+print(text1)
+print(text2)
+print(text3)
+print(long_text)
+print(ecs_char)
