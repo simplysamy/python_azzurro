1e27a30e4cf2ee76efa17e5aca240d04cde51bed
Sameer Hasan <simplysamy@gmail.com>
2024-11-13 19:44:17 +0530

    tuple1

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-people: tuple = ('John', 'Paul', 'George', 'Ringo') #Best practice to use pranthesis
+people: tuple = ('John', 'Paul', 'George', 'Ringo') # Best practice to use pranthesis
 
-# people: tuple = 'John', 'Paul', 'George', 'Ringo' 
-# [Paranthesis don't make a tuple. it's the comma in the end ] 
-# people = 'sam', [This is also a tuple]
+people[0] = 'sam' # Error 'tuple' object does not support item assignment
 
-people = 'sam',
-print(type(people))
+print (people) 
