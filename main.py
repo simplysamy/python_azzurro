e903676f20b65877957191515ee54a403cc2ae4b
Sameer Hasan <simplysamy@gmail.com>
2024-11-13 20:17:12 +0530

    print element of tuple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 people: tuple = ('John', 'Paul', 'George', 'Ringo') # Best practice to use pranthesis
 
-people_list: list[str] = list(people) # change tuple to list
-
-print (people_list) 
+print(people[2:]) # Print elements from index 2 to the end
+print(people[:2]) # Print elements below 2
