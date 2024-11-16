f9f00c7b325aea8f6899ed761ae0ac7a3c5b9030
Sameer Hasan <simplysamy@gmail.com>
2024-11-16 19:44:25 +0530

    set4-clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 items: set = {'apple', 'banana', 10, True}
 
-#items.remove(100) # of iterm doesn't exist, it will throw an error
+items.clear()
 
-items.discard(100) # of iterm doesn't exist, it will not throw an error
 print(items)
