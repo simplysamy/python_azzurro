1e92672b1b7f624d2e4ca3e704b23722443e4c51
Sameer Hasan <simplysamy@gmail.com>
2024-11-16 21:02:31 +0530

    dict3 - not throw an error if the key is not found

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 users = {'user1':'mario123',
          'user2':'luigi123,'}
 
-user1 = users['user1']
+# user1 = users['user1'] # throws an error if user1 does not exist
+
+user1 = users.get('user3') ## does not throw an error 
 
 print(user1)
