9cfeaa95694394f346ba3a83b534d65cf1906d70
Sameer Hasan <simplysamy@gmail.com>
2024-11-16 20:18:58 +0530

    set8-Updates the current set & provide the output in the new set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 items1: set = {'apple', 'banana', 10, True}
 items2: set = {'apple', 10, False, 14}
-
-items1.symmetric_difference_update(items2)
+items1.symmetric_difference_update(items2) # Updates the current set
 print(items1)
+
+new_set =items1.symmetric_difference(items2) # Updates the current set & provide the output in the new set
+print(new_set)
