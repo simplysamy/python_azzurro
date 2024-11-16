bd308e189a5aa34815ba273c71ac987283b27e19
Sameer Hasan <simplysamy@gmail.com>
2024-11-16 21:04:35 +0530

    dict4 - print keys & values

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 users = {'user1':'mario123',
          'user2':'luigi123,'}
 
-# user1 = users['user1'] # throws an error if user1 does not exist
-
-user1 = users.get('user3') ## does not throw an error 
-
-print(user1)
+x = users.keys()
+y = users.values()
+print(x)
+print(y)
