4449c42379150ad6808380b982824153dbdb7724
Sameer Hasan <simplysamy@gmail.com>
2024-11-23 19:00:19 +0530

    dict13-Dict inside Dictionary

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 users = {'user1':'mario123',
-         'user2':'luigi123,'}
-
-users.clear() 
+         'user2':'luigi123',
+         'items': {'apple': 10, 
+                   'banana': 5}}   # dictionary inside a dictionary
+ 
 
 print(users)
+print(users['items'])
+print(users['items']['apple'])
