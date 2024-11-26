06574d0630a5f627743e2b1ee1ebc23d3eb12b87
Sameer Hasan <simplysamy@gmail.com>
2024-11-26 18:53:17 +0530

    for loop - using range

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for letter in 'mario':
-    print(letter)
+print('Print out values in a range')
+
+for i in range(0, 10):
+   print(i, '', end = '') 
+#  end = '' changes that behavior, so the next print output is 
+#  appended to the same line without adding a newline.
+  
+
+print('\nDone')
