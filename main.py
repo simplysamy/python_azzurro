76bd93c0e5a9da2f19f81b7c36e4bcdb0ec40165
Sameer Hasan <simplysamy@gmail.com>
2024-11-27 09:26:14 +0530

    for loop  -Anonymous  Variables

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# Now use values in a range but increment by 2
-print('Print out values in a range with an increment of 2')
-
-for i in range(0, 10, 2):
- print(i, ' ', end='')
- 
-print('\nDone')
+# Now use an ’anonymous’ loop variable
+for _ in range(0,10):
+    print('.', end='')
