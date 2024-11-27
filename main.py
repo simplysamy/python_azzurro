e3c0c9c6b810eba178abe4fbf0b8ee0f64108e25
Sameer Hasan <simplysamy@gmail.com>
2024-11-27 10:32:45 +0530

    function1

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-i = 0
-while  i < 3:
-    print(i, end=' ')
-    i += 1
-    
-    if i == 2:
-        break
+def print_msg():
+  print('Hello World!')
 
-else:
-    print('Sucessful')
+print_msg()
