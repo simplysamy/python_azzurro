522aabd2b3a61326852d2a9d77310aebc2c49e87
Sameer Hasan <simplysamy@gmail.com>
2024-11-29 15:31:55 +0530

    function - recursion

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-def square(n):
-  return n * n
-
-print(square(2))
-
-# Store result from square in a variable
-result = square(4)
-print(result)
-
-# Send the result from square immediately to another function
-print(square(5))
-
-# Use the result returned from square in a conditional expression
-if square(3) < 15:
-    print(f'{square(3)} is less than 15')
+def do_something(n: int):
+    print(n)
+    
+    if n==1:
+        print("done with the function")
+        return
+    
+    do_something(n-1) # recursive call - function calls itself
+    
+do_something(3)
+    
