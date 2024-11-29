8c0a798d61227463dac7ffe01b058776e85779dd
Sameer Hasan <simplysamy@gmail.com>
2024-11-29 15:22:22 +0530

    function - return statement

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def greeter(*args): # do not know how many arguments will be supplied when a function is called. 
-   for name in args:
-      print('Welcome', name)
+def square(n):
+  return n * n
 
-greeter('John', 'Denise', 'Phoebe', 'Adam', 'Gryff', 'Jasmine')
+print(square(2))
+
+# Store result from square in a variable
+result = square(4)
+print(result)
+
+# Send the result from square immediately to another function
+print(square(5))
+
+# Use the result returned from square in a conditional expression
+if square(3) < 15:
+    print(f'{square(3)} is less than 15')
