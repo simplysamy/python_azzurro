25d4c128c19cb5a618070fe1facf3c8c04c1932e
Sameer Hasan <simplysamy@gmail.com>
2024-12-04 18:47:52 +0530

    error_handling5

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-user_input = input("Enter a number: ")
+def do_math():
+    user_input = input("Enter a number: ")
 
-try:
-    number = float(user_input)
-    result = number / 0
-    print(number)
-    
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
-    
-except ZeroDivisionError:
-    print("Please don't divide by zero!")
-    
-except Exception as e:
-    print(f"An error occurred: {e}")
+    try:
+        number = float(user_input)
+        print(number)
+        
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        
+        do_math() # recursion
+        
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
