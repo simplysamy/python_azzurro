80147b6c8b3fc86105dad0ba59a1835af6b46188
Sameer Hasan <simplysamy@gmail.com>
2024-12-04 18:43:28 +0530

    error_handling4

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ user_input = input("Enter a number: ")
 
 try:
     number = float(user_input)
+    result = number / 0
     print(number)
     
 except ValueError:
     print("Invalid input. Please enter a valid number.")
+    
+except ZeroDivisionError:
+    print("Please don't divide by zero!")
+    
+except Exception as e:
+    print(f"An error occurred: {e}")
