eef2c7bab9cfc243379c7200cc8e3f937d54e391
Sameer Hasan <simplysamy@gmail.com>
2024-12-08 15:01:45 +0530

    enum2

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class state(Enum):
 
 state_of_lamp = state.OFF
 
+print(f'The state of the lamp is', state.OFF.value) # Get the value of the state of the lamp
+
 if state_of_lamp == state.ON:
     print("The lamp is on")
 else:
