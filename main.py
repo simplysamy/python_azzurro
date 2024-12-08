311bfa609d13469b0d39755116fc7ebbc3d66907
Sameer Hasan <simplysamy@gmail.com>
2024-12-08 15:53:58 +0530

    comparing_floats2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 a = 0.1 + 0.2  # Result is slightly more than 0.3
 b = 0.3
-epsilon = 1e-10  # Set a small tolerance level
+epsilon = 1e-17  # Set a small tolerance level
 
 # Standard comparison
 print(a == b)  # This will output False
 
 # Using tolerance level
 if abs(a - b) < epsilon:
-    print("a is approximately equal to b")  # This will be printed
+    print("a is approximately equal to b")
 else:
-    print("a is not equal to b") 
+    print("a is not equal to b")  # This will be printed
