432884e6a64e29617c3f506abe6c870091a50ec2
Sameer Hasan <simplysamy@gmail.com>
2024-12-08 14:51:53 +0530

    truthy

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# If it contains no value, then it is empty & evaluate to False
+# Examples of truthy values
+if [1, 2, 3]:
+    print("Non-empty list is truthy")  # This will be printed
 
-# Examples of falsy values
-if not []:
-    print("Empty list is falsy")  # This will be printed
+if "hello":
+    print("Non-empty string is truthy")  # This will be printed
 
-if not "":
-    print("Empty string is falsy")  # This will be printed
+if 42:
+    print("Non-zero number is truthy")  # This will be printed
 
-if not 0:
-    print("Zero is falsy")  # This will be printed
-
-if not None:
-    print("None is falsy")  # This will be printed
+if {"key": "value"}:
+    print("Non-empty dictionary is truthy")  # This will be printed
