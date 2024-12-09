ab678a41df1551366400e23ccdf9775b1607ef48
Sameer Hasan <simplysamy@gmail.com>
2024-12-09 19:43:50 +0530

    nested function with non-local

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def outer_function():
-    x = 10
+    x = 10  # Variable in outer function's scope
     
     def inner_function():
-        x = 5  # This creates a new local variable x in inner_function
+        nonlocal x  # Indicates that we want to use the x in the outer_function
+        x += 5  # Modify the outer function's variable
         print("Inner function, x =", x)
     
-    inner_function()
-    print("Outer function, x =", x)
+    inner_function()  # Call inner_function
+    print("Outer function, x =", x)  # Check the value of x in the outer function
 
 outer_function()
