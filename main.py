4423c28102dd0acc774e2068bf06040fe0293bf3
Sameer Hasan <simplysamy@gmail.com>
2024-12-09 19:48:42 +0530

    parctical non local example

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def outer_function():
-    x = 10  # Variable in outer function's scope
+    count = 0  # A variable to keep track of counts
     
     def inner_function():
-        nonlocal x  # Indicates that we want to use the x in the outer_function
-        x += 5  # Modify the outer function's variable
-        print("Inner function, x =", x)
-    
-    inner_function()  # Call inner_function
-    print("Outer function, x =", x)  # Check the value of x in the outer function
+        nonlocal count  # Indicating we will modify count in the outer_function
+        count += 1  # Increment the count
+        print("Count in inner function:", count)
+
+    inner_function()  # Call it a few times
+    inner_function()
+    inner_function()
 
 outer_function()
