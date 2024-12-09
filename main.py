11eccb5bb804003c4e3d125c4cc623eb6c1b5b91
Sameer Hasan <simplysamy@gmail.com>
2024-12-09 18:47:45 +0530

     if __name__ == "__main__"1

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-price1 = 19.99
-price2 = 19.99 + 0.0000000001  # Slightly more than 19.99
-epsilon = 1e-10  # Set a very tight tolerance level
+import sample_module
 
-if abs(price1 - price2) < epsilon:
-    print("Prices are approximately equal")  # This will be printed
-else:
-    print("Prices are not equal")
+print("Code is running")
---
+++ sample_module.py
@@ -0,0 +1,5 @@
+def do_something():
+    print("Hello from sample_module")   
+    
+
+do_something()
